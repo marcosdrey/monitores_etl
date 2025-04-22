6080c2e03bcdfd515596dc5eeb598e33b572969f
marcosdrey <programmarcos05@gmail.com>
2025-04-22 12:51:22 -0300

    chore: add dashboard subheader tests

--- tests/test_dashboard.py
+++ tests/test_dashboard.py
@@ -5,3 +5,11 @@ def test_app_starts(at):
 def test_page_title(at):
     assert len(at.header) == 1
     assert "📊 Pesquisa - Monitores Gamer no Mercado Livre" == at.header[0].value
+
+
+def test_page_subheaders(at):
+    assert len(at.subheader) == 4
+    assert "💡 Principais KPIs" == at.subheader[0].value
+    assert "🔍 Marcas mais encontradas" == at.subheader[1].value
+    assert "💵 Preço médio por marca" == at.subheader[2].value
+    assert "💡 Satisfação média por marca" == at.subheader[3].value
